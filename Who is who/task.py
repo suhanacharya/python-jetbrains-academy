541c06438e3d7d85d33133287e5e6fda03e2d365
suhanacharya <suhan.acharya1@gmail.com>
2020-05-06 22:24:56 +0530

    Added program

---
+++ Who is who/task.py
@@ -0,0 +1,10 @@
+# mystery_entity has already been defined
+# print its class attributes
+
+# if mystery_entity.color == "white" or mystery_entity.feature = "wings"
+#         or mystery_entity.home = "Heaven":
+#     print
+
+print(mystery_entity.color)
+print(mystery_entity.feature)
+print(mystery_entity.home)
