6e7e18d737ab4fdc6495113dc9384155e67fa712
suhanacharya <suhan.acharya1@gmail.com>
2020-05-28 23:24:45 +0530

    Added small programs

---
+++ Modeling situation/task.py
@@ -0,0 +1,9 @@
+try:
+	exception_test()
+except AssertionError:
+	print("AssertionError")
+except ZeroDivisionError:
+	print("ZeroDivisionError")
+except ArithmeticError:
+	print("ArithmeticError")
+
